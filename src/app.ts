95c354c5593b0d19447be6ca6d71799f80c6d0b9
Darrel-Day Guerrero <ddayy.guerrero@hotmail.com>
2015-09-12 15:02:44 -0400

    app

--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,17 @@ import { Component, View } from 'angular2/angular2';
 
 // Annotation section
 @Component({
-  selector: 'app'
+  selector: 'app' 
 })
+
 @View({
   template: `<h1>Test-Driven Development with Angular 2<h1/>
   	    <h2>Article Description </h2>
-	    <p>Title: {{ title }}</p> 
-	    <p>Authors: {{ authors }}</p> 
+        <p>Title: {{ title }} </p> 
+        <p>Authors: {{ authors }} </p> 
   	    `
 })
+
 // Component controller
 export class ArticleComponent {
   title: string;
@@ -20,8 +22,8 @@ export class ArticleComponent {
   
   constructor() {
   	this.title = "TDD with Angular2";
-	this.authors = "Darrel and Daniel";
-	this.tags = ["TDD", "Angular2"];
+	  this.authors = "Darrel and Daniel";
+	  this.tags = ["TDD", "Angular2"];
   }
 
   addTag(tag: string){
