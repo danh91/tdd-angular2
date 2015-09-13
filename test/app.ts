e2ac88f9a65dec1fc0e93c7cdf006d5b83b778b6
DanielK <danielk.developer@gmail.com>
2015-09-13 14:36:16 -0400

    fix run npm commands errors

--- test/app.ts
+++ test/app.ts
@@ -2,29 +2,27 @@
 /// <reference path="../typings/chai/chai.d.ts" />
 /// <reference path="../typings/angular2/angular2.d.ts" />
 
-require('reflect-metadata');
-import angular2 = require('angular2/angular2');
+import 'reflect-metadata';
+import 'angular2/angular2';
 
-import chai = require('chai');
+import { expect } from 'chai';
 
-var expect = chai.expect;
-
-var App = require('../build/app');
+import { ArticleComponent } from  '../src/app';
 
 describe('Article model Unit Tests:', () => {
 
     describe('default value', () => {
         it('should create a new article component', (done) => {
-            var article = new App.ArticleComponent();
+            var article = new ArticleComponent();
             expect(article.title).to.be.equals("TDD with Angular2");
             done();
         });
     });
-    
+
     describe('Add tag method', () => {
         it('should add a new tag to the article', (done) => {
-            var article = new App.ArticleComponent();
-	    article.addTag("Unit testing");
+            var article = new ArticleComponent();
+            article.addTag("Unit testing");
             expect(article.tags.length).to.be.equals(3);
             done();
         });
