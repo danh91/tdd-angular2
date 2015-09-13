e306f963bc963e84a6ea082b61e432ed772c8cfc
DanielK <danielk.developer@gmail.com>
2015-09-13 14:36:16 -0400

    fix run npm commands errors

--- build/main.js
+++
@@ -1,4 +0,0 @@
-/// <reference path="../typings/angular2/angular2.d.ts" />
-var angular2_1 = require('angular2/angular2');
-var app_1 = require('./app');
-angular2_1.bootstrap(app_1.ArticleComponent);
